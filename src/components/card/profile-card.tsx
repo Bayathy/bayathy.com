44366dbb368bfd6121107f381f85ea93990b032a
Bayathy <misosoup0321@icloud.com>
2022-12-16 10:06:42 +0900

    feat: create ProfileSection file

--- src/components/card/profile-card.tsx
+++
@@ -1,9 +0,0 @@
-import { Paper, Title } from '@mantine/core'
-
-import { FC } from 'react'
-
-export const ProfileCard: FC = () => (
-   <Paper shadow={"xs"} p="md">
-      <Title>Profile</Title>
-   </Paper>
-)
