4b537801ce16cd8cec8ffd5639c56765821295cb
Bayathy <misosoup0321@icloud.com>
2022-12-18 19:22:20 +0900

    chore: wip

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
-import { Text, MantineProvider } from '@mantine/core'
+import { Container, MantineProvider } from '@mantine/core'
+import { ProfileSection } from './components/section/profile-section'
+import { Header } from './components/header'
 
 export default function App() {
    return (
       <MantineProvider withGlobalStyles withNormalizeCSS>
-         <Text>Welcome to Mantine!</Text>
+         <Header title={'Logo'} />
+         <Container>
+            <ProfileSection />
+         </Container>
       </MantineProvider>
    )
 }
---
+++ src/components/section/profile-section.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react'
+import { Flex, Title } from '@mantine/core'
+
+export const ProfileSection: FC = () => (
+   <section>
+      <Flex h="30vh" align="center">
+         <Title>Keito Kobayashi</Title>
+      </Flex>
+   </section>
+)
