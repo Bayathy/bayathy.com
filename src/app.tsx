ded80898a551a7327423d4d60a06f59cf22d5c25
Bayathy <misosoup0321@icloud.com>
2022-12-19 05:53:20 +0900

    chore: add Change some files

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,27 @@ import { Container, MantineProvider } from '@mantine/core'
 import { ProfileSection } from './components/section/profile-section'
 import { Header } from './components/header'
 
+const timeline = [
+   { text: 'test', title: ' test' },
+   { text: 'test', title: ' test' },
+   { text: 'test', title: ' test' }
+]
+
+const profileList = [
+   { index: 'test', text: ' test' },
+   { index: 'test', text: ' test' },
+   { index: 'test', text: ' test' }
+]
 export default function App() {
    return (
       <MantineProvider withGlobalStyles withNormalizeCSS>
          <Header title={'Logo'} />
          <Container>
-            <ProfileSection />
+            <ProfileSection
+               title={'Bayathy'}
+               timeline={timeline}
+               profileList={profileList}
+            />
          </Container>
       </MantineProvider>
    )
