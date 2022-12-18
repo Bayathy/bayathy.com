0e33e641184a86358f9b3717bd2fc86f52a2b09d
Bayathy <misosoup0321@icloud.com>
2022-12-19 05:52:56 +0900

    feat: create ProfileSection

--- src/components/section/profile-section.tsx
+++ src/components/section/profile-section.tsx
@@ -1,11 +1,24 @@
 import type { FC } from 'react'
-import { Flex, Text, Title } from '@mantine/core'
+import { Flex, Stack, Title } from '@mantine/core'
+import { TimelineCard, TimelineCardProps } from '../cards/timeline-card'
+import { ProfileCard, ProfileCardProps } from '../cards/profile-card'
 
-export const ProfileSection: FC = () => (
+export type ProfileSectionProps = {
+   title: string
+} & ProfileCardProps &
+   TimelineCardProps
+export const ProfileSection: FC<ProfileSectionProps> = ({
+   title,
+   timeline,
+   profileList
+}) => (
    <section>
       <Flex h="30vh" align="center" justify="center">
-         <Title>Keito Kobayashi</Title>
-         <Text></Text>
+         <Title>{title}</Title>
       </Flex>
+      <Stack>
+         <ProfileCard profileList={profileList} />
+         <TimelineCard timeline={timeline} />
+      </Stack>
    </section>
 )
