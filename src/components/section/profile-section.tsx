a5b5901eaa39002b1ee2f2b8e5ace544fb33ee02
Bayathy <misosoup0321@icloud.com>
2022-12-18 21:45:02 +0900

    feat: create Profile Section

--- src/components/section/profile-section.tsx
+++ src/components/section/profile-section.tsx
@@ -1,10 +1,11 @@
 import type { FC } from 'react'
-import { Flex, Title } from '@mantine/core'
+import { Flex, Text, Title } from '@mantine/core'
 
 export const ProfileSection: FC = () => (
    <section>
-      <Flex h="30vh" align="center">
+      <Flex h="30vh" align="center" justify="center">
          <Title>Keito Kobayashi</Title>
+         <Text></Text>
       </Flex>
    </section>
 )
